eb4190a460416482677e530947595bdef0700443
Usagi88 <ibrahimnaish5677@gmail.com>
2022-03-12 18:48:10 +0500

    profile view

--- src/components/Views/Profile.tsx
+++ src/components/Views/Profile.tsx
@@ -1,8 +1,189 @@
+import {faUserAlt} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import {View} from 'react-native';
+import {
+  Dimensions,
+  ImageBackground,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import {IconButton} from 'react-native-paper';
+import {CountUp} from 'use-count-up';
+import Navbar from '../common/Navbar';
+import TitleHorizonDivider from '../common/TitleHorizonDivider';
+import BannerWalletIcon from '../SvgIconComponents/BannerWalletIcon';
+import PencilEditIcon from '../SvgIconComponents/PencilEditIcon';
 
 const Profile = () => {
-  return <View></View>;
+  return (
+    <>
+      <Navbar />
+      <ScrollView>
+        <View style={styles.column}>
+          <ImageBackground
+            source={require('../../assets/images/banner.png')}
+            style={{height: 160, width: width}}
+          />
+          <View style={styles.bannerRow}>
+            <FontAwesomeIcon icon={faUserAlt} size={44} color={'white'} />
+
+            <View style={{marginLeft: 20}}>
+              <Text style={styles.textStyle}>Test Name</Text>
+              <Text style={styles.textStyle}>A123456</Text>
+            </View>
+          </View>
+          <TitleHorizonDivider name={'Profile'} />
+          <View style={styles.listTitleWrapper}>
+            <View style={styles.userInfoBox}>
+              <Text style={styles.title}>Email</Text>
+              <Text style={styles.subtitle}>test@gmail.com</Text>
+            </View>
+            <IconButton
+              icon={() => (
+                <PencilEditIcon width={24} height={24} color={'#25BFA3'} />
+              )}
+              size={24}
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{paddingHorizontal: 20,}}>
+            <View style={styles.divider}/>
+          </View>
+          
+
+          <View style={styles.listTitleWrapper}>
+            <View style={styles.userInfoBox}>
+              <Text style={styles.title}>Mobile Number</Text>
+              <Text style={styles.subtitle}>7777777</Text>
+            </View>
+            <IconButton
+              icon={() => (
+                <PencilEditIcon width={24} height={24} color={'#25BFA3'} />
+              )}
+              size={24}
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{paddingHorizontal: 20,}}>
+            <View style={styles.divider}/>
+          </View>
+
+          <View style={styles.listTitleWrapper}>
+            <View style={styles.userInfoBox}>
+              <Text style={styles.title}>Address</Text>
+              <Text style={styles.subtitle}>test address</Text>
+            </View>
+            <IconButton
+              icon={() => (
+                <PencilEditIcon width={24} height={24} color={'#25BFA3'} />
+              )}
+              size={24}
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{paddingHorizontal: 20,}}>
+            <View style={styles.divider}/>
+          </View>
+
+          <View style={styles.listTitleWrapper}>
+            <View style={styles.userInfoBox}>
+              <Text style={styles.title}>Island/City</Text>
+              <Text style={styles.subtitle}>test island/city</Text>
+            </View>
+            <IconButton
+              icon={() => (
+                <PencilEditIcon width={24} height={24} color={'#25BFA3'} />
+              )}
+              size={24}
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{paddingHorizontal: 20,}}>
+            <View style={styles.divider}/>
+          </View>
+
+          <View style={styles.listTitleWrapper}>
+            <View style={styles.userInfoBox}>
+              <Text style={styles.title}>Country</Text>
+              <Text style={styles.subtitle}>test country</Text>
+            </View>
+            <IconButton
+              icon={() => (
+                <PencilEditIcon width={24} height={24} color={'#25BFA3'} />
+              )}
+              size={24}
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{paddingHorizontal: 20,}}>
+            <View style={styles.divider}/>
+          </View>
+
+          <View style={styles.listTitleWrapper}>
+            <View style={styles.userInfoBox}>
+              <Text style={styles.title}>Post Code</Text>
+              <Text style={styles.subtitle}>test post code</Text>
+            </View>
+            <IconButton
+              icon={() => (
+                <PencilEditIcon width={24} height={24} color={'#25BFA3'} />
+              )}
+              size={24}
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{paddingHorizontal: 20,}}>
+            <View style={styles.divider}/>
+          </View>
+        </View>
+      </ScrollView>
+    </>
+  );
 };
 
 export default Profile;
+
+let {height, width} = Dimensions.get('window');
+const styles = EStyleSheet.create({
+  column: {
+    width: width,
+    minHeight: height - 56,
+    backgroundColor: 'white',
+  },
+  bannerRow: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    //backgroundColor: 'yellow',
+    width: width,
+    top: 30,
+    minHeight: 62,
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  listTitleWrapper: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'black',
+    paddingBottom: 5,
+  },
+  subtitle: {
+    color: '#858b94',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#ccc',
+    marginVertical: 20,
+  }
+});
